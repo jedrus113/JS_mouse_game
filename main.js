4d7569f41153b4db0f998addeca66936015e20ee
Andrew Dabski <jedrus1133@gmail.com>
2017-12-26 05:01:22 +0100

    init commit with all my work working dot chaging coin using mouse movement

---
+++ main.js
@@ -0,0 +1,93 @@
+var gameInterval;
+var gameCanvas;
+var eventCatcherDiv;
+var heroX;
+var heroY;
+var coinX = 100;
+var coinY = 100;
+
+function startLoading()
+{
+    eventCatcherDiv = document.getElementById("EventCatcher");
+    // eventCatcherDiv events go here
+
+    gameCanvas = document.getElementById("GraphicsBox");
+    drawText(gameCanvas.getContext("2d"), "Loading...", true, 50, 50);
+
+    gameInterval = setInterval(hasLoaded, 250);
+}
+
+function hasLoaded()
+{
+
+    eventCatcherDiv.addEventListener("mousemove", canvasMove);
+    if (true) // Check to see if all info is loaded
+    {
+        clearInterval(gameInterval);
+        startGame();
+    }
+}
+
+function drawText(g, stringValue, fillText, x, y)
+{
+    g.font = "30px Arial";
+    g.fillStyle = "#00FF00";
+    if (fillText)
+        g.fillText(stringValue, x, y);
+    else
+        g.strokeText(stringValue, x, y);
+}
+
+function drawCoin(g)
+{
+    g.fillStyle = "#FFD700";
+    g.beginPath();
+    g.arc(coinX, coinY, 20, 0, 2*Math.PI);
+    g.fill();
+}
+
+function drawTheHero(g)
+{
+    g.fillStyle = "#0000FF";
+    g.fillRect(heroX, heroY, 20, 20);
+}
+
+function startGame()
+{
+    heroX = 10;
+    heroY = 10;
+    drawTheHero(gameCanvas.getContext("2d"));
+    gameInterval = setInterval(runGame, 25);
+}
+
+function canvasMove(E)
+{
+    E = E || window.event;
+    heroX = E.pageX;
+    heroY = E.pageY;
+}
+
+function runGame()
+{
+    if (isTouchingCoin())
+        moveCoin();
+    gameCanvas.getContext("2d").clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+    drawCoin(gameCanvas.getContext("2d"));
+    drawTheHero(gameCanvas.getContext("2d"));
+
+}
+
+function isTouchingCoin()
+{
+    // 20x20 is the size of the hero.
+    // The radius of the coin is 20.
+    if (heroX + 20 > coinX - 20 && heroX < coinX + 20
+        && heroY + 20 > coinY - 20 && heroY < coinY + 20)
+        return true;
+}
+
+function moveCoin()
+{
+    coinX = Math.random() * 600; // width of draw area
+    coinY = Math.random() * 400; // height of draw area
+}
