040c8b7ba134d1858375690c4f3e1f9e3974d21a
Dari PC <Dari@PC>
2018-01-01 20:01:35 +0100

    fix messages to snowman game

--- screens.js
+++ screens.js
@@ -1,9 +1,20 @@
 let gameOverImageIndex;
+let averageScoreMessage = "Average Score: ";
+let fastPointsMessage = "Full snowmans: ";
+let slowPointsMessage = "Half-melted snowmans: ";
+let enemiesLeftMessage = "Melted snowmans: ";
+let gameOverReasonMessage = "Melted by: ";
+let emailaddress = "dabski.andrzej@gmail.com";
 
 function loadingScreens(){
     let gameOverImageSrc = "images/Santa/gameOver.png";
     if (MEG_VERSION){
         gameOverImageSrc = "images/MEG/gameOver.png";
+        averageScoreMessage = "Avg Audit Score: ";
+        fastPointsMessage = "Good Audits: ";
+        slowPointsMessage = "OK Audits: ";
+        enemiesLeftMessage = "Bad Audits Left: ";
+        gameOverReasonMessage = "Death reason: ";
     }
 
     gameOverImageIndex = images.length;
@@ -27,13 +38,13 @@ function showGameOverScreen(reason){
     let auditAverage = Math.round(score / (eatenFreeshFood + eatenRootenFood));
 
     let fontSize = 18;
-    drawText(g, "Avg Audit Score: " + auditAverage, true, fontSize, 238, 155);
+    drawText(g, averageScoreMessage + auditAverage, true, fontSize, 238, 155);
     drawText(g, "Time Survived: " + secondAlive + "s", true, fontSize, 220, 180);
-    drawText(g, "Good Audits: " + eatenFreeshFood, true, fontSize, 220, 205);
-    drawText(g, "OK Audits: " + eatenRootenFood, true, fontSize, 220, 230);
-    drawText(g, "Bad Audits Left: " + enemyTable.length, true, fontSize, 220, 255);
-    drawText(g, "Death reason: " + reason, true, fontSize, 220, 280);
+    drawText(g, fastPointsMessage + eatenFreeshFood, true, fontSize, 220, 205);
+    drawText(g, slowPointsMessage + eatenRootenFood, true, fontSize, 220, 230);
+    drawText(g, enemiesLeftMessage + enemyTable.length, true, fontSize, 220, 255);
+    drawText(g, gameOverReasonMessage + reason, true, fontSize, 220, 280);
 
     drawText(g, "Programmer: Andrzej Dąbski", true, 15, 215, 385);
-    drawText(g, "andrew@medicaleguides.com", true, 15, 215, 397);
+    drawText(g, dabski.andrzej@gmail.com, true, 15, 215, 397);
 }
